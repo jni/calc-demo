04b2f833e74745c608c6e6bae542e3f7caf30f84
Simon Gladman (Monash University) <slugger@merri.pcf.vlsci.unimelb.edu.au>
2013-11-25 12:14:02 +1100

    modified to suit comments mult function

--- calc.py
+++ calc.py
@@ -2,10 +2,9 @@ import sys
 
 if __name__ == '__main__':
     cmd = sys.argv[1]
+    nums = map(float, sys.argv[2:])
     if cmd == "add":
-        nums = map(float, sys.argv[2:])
         print(sum(nums))
     elif cmd == "mult":
-        nums = map(float, sys.argv[2:])
         print(reduce(lambda a,b:a*b, nums))
 

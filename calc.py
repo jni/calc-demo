c2c2891f01aa3ebab54b0d2e729bfc28b5c075c2
Simon Gladman (Monash University) <slugger@merri.pcf.vlsci.unimelb.edu.au>
2013-11-25 12:04:38 +1100

    add mult function

--- calc.py
+++ calc.py
@@ -1,6 +1,11 @@
 import sys
 
 if __name__ == '__main__':
-    nums = map(float, sys.argv[1:])
-    print(sum(nums))
+    cmd = sys.argv[1]
+    if cmd == "add":
+        nums = map(float, sys.argv[2:])
+        print(sum(nums))
+    elif cmd == "mult":
+        nums = map(float, sys.argv[2:])
+        print(reduce(lambda a,b:a*b, nums))
 
